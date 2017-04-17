ed9b8dc1e7d131a26d1a4538d6561b430210897a
Aurelio Grott Neto <grott.aurelio@gmail.com>
2017-04-17 20:57:35 -0300

    all ok.

--- aloca.jl
+++ aloca.jl
@@ -1,9 +1,12 @@
+include("dijkstra.jl")
+
 MAX_BANDWIDTH = 50
 MIN_BANDWIDTH = 10
 Σ = sum
 Π = prod
 
 type VirtualMachine
+    id::Int64
     cp::Float64
     storage::Float64
     memory::Float64
@@ -21,6 +24,7 @@ type Server
     cp::Resource # adicionar parametro alfa de overbooking
     storage::Resource
     memory::Resource
+    residual_band::Float64
     VMs::Array{VirtualMachine}
     custo_beneficio::Float64
 end
@@ -74,14 +78,14 @@ end
 function MAUT_norm_criteria_max(array_criteria::Array{Float64})
      min = minimum(array_criteria)
      max = maximum(array_criteria)
-     ↑ = (x-> (x - min)/(max-min))
+     ↑ = (min != max) ? (x-> (x - min)/(max-min)) : (x-> 1.0)
      return copy!(array_criteria, pmap(↑, array_criteria))
 end
 
 function MAUT_norm_criteria_min(array_criteria::Array{Float64})
     min = minimum(array_criteria)
     max = maximum(array_criteria)
-    ↓ = (x-> 1+(min-x)/(max - min))
+    ↓ = (min != max) ? (x-> 1+(min-x)/(max - min)) : (x-> 1.0)
     return copy!(array_criteria, pmap(↓, array_criteria))
 end
 
@@ -103,99 +107,112 @@ function MAUT_globalUtility(array_criterias, pesos)
     return result
 end
 
-function parallel_calc_cp_residual(server, vm_cp)
+function parallel_calc_cp_residual(server::Server, vm_cp::Float64)
     server.cp.residual = server.cp.atual - vm_cp
 end
 
-function calc_cp_residual(Gs::Array{Server},vm::VirtualMachine)
+function calc_cp_residual(Gs::Array{Server, 1},vm::VirtualMachine)
     return pmap(x->parallel_calc_cp_residual(x,vm.cp),Gs)
 end
 
-function parallel_calc_storage_residual(server, vm_storage)
+function parallel_calc_storage_residual(server::Server, vm_storage::Float64)
     server.storage.residual = server.storage.atual - vm_storage
 end
 
-function calc_storage_residual(Gs::Array{Server},vm::VirtualMachine)
+function calc_storage_residual(Gs::Array{Server, 1},vm::VirtualMachine)
     return pmap(x->parallel_calc_storage_residual(x,vm.storage),Gs)
 end
 
-function parallel_calc_memory_residual(server, vm_memory)
+function parallel_calc_memory_residual(server::Server, vm_memory::Float64)
     server.memory.residual = server.memory.atual - vm_memory
 end
 
-function calc_memory_residual(Gs::Array{Server},vm::VirtualMachine)
+function calc_memory_residual(Gs::Array{Server, 1}, vm::VirtualMachine)
     return pmap(x->parallel_calc_memory_residual(x,vm.memory),Gs)
 end
 
-function calc_custo_beneficio(Gs::Array{Server}, BandM::Array{Float64},vnr::VirtualMachine)
+function calc_custo_beneficio(Gs::Array{Server, 1}, BandM::Array{Float64, 2}, vnr::VirtualMachine)
     Band_v = Σ([link[2] for link in vnr.links])
     for i=1:length(Gs)
-        Gs[i].custo_beneficio = (vnr.cp / Gs[i].cp.atual) + (Band_v / Σ([k for k in BandM[i,1:size(BandM,1)]]))
+        if((Gs[i].cp.atual == 0) || (Gs[i].residual_band == 0))
+            Gs[i].custo_beneficio = 0    
+        else
+            Gs[i].custo_beneficio = (vnr.cp / Gs[i].cp.atual) + (Band_v / Gs[i].residual_band)
+        end
     end
 end
 
-function index_of_higher_value_element(array::Array{Float64})
+function index_of_higher_value_element(array::Array{Float64, 1})
     index = 0
     higher_value = -1
+    println("\tanalisando ", array)
     for i=1:length(array)
         if (array[i] > higher_value)
             index = i
             higher_value = array[i]
         end
     end
+    println("\tindex de maior valor ", index)
     return index
 end
 
-function do_allocate(Gs::Array{Server}, selected_server::Int64, VM::VirtualMachine)
+function all_positive_residual_values_in(node::Server, VM::VirtualMachine)
+    if (node.cp.residual < 0)
+        return false
+    end
+    if (node.storage.residual < 0)
+        return false
+    end
+    if (node.memory.residual < 0)
+        return false
+    end
+    if (node.residual_band - Σ([link[2] for link in VM.links]) < 0)
+        return false
+    end
+    return true
+end
+
+function do_allocate(Gs::Array{Server, 1}, selected_server::Int64, VM::VirtualMachine)
+    ret = false
     for i=1:length(Gs)
-        if (i == selected_server)
+        if (i == selected_server && all_positive_residual_values_in(Gs[i], VM))
             Gs[i].cp.atual = Gs[i].cp.residual
+            if (Gs[i].cp.atual == 0)
+                println(">>>>>>>>>> atual e residual ", Gs[i].cp.atual, Gs[i].cp.residual, selected_server)
+            end
             Gs[i].storage.atual = Gs[i].storage.residual
             Gs[i].memory.atual = Gs[i].memory.residual
             push!(Gs[i].VMs, VM)
+            Gs[i].residual_band -= Σ([link[2] for link in VM.links])
+            ret = true
         end
         Gs[i].storage.residual = 0
         Gs[i].cp.residual = 0
         Gs[i].memory.residual = 0
         Gs[i].custo_beneficio = 0
     end
+    return ret
 end
 
-#C_{r}(p) = c_{p} - c_{v}, sendo:
-#C_{r}(p) a capacidade cp_residual de p;
-#c_{p} = capacidade original de p (o total);
-#c_{v} = capacidade solicitada pelo cliente (virtual)
-
-#####################################
-# TESTE DINAMICO
-# MATRIZES GERADAS ALEATORIAMENTE
-#
-
-# n_nodes =5
-
-# Gs,BandM = gera_Gs_BandM(n_nodes)
-# println(Gs, "\n", BandM)
-
-# MAUT_criteria_custo_beneficio = [Float64(k.custo_beneficio) for k in Gs]
-# MAUT_criteria_cp_residual = [Float64(k.cp_residual) for k in Gs]
-
-# println("custo: ", MAUT_criteria_custo_beneficio, "\ncp_residual: ", MAUT_criteria_cp_residual, "")
-# VNR  = gera_VNR(3)
-
-# #println("\n", VNR, "\n")
-
-# for node in VNR
-#     println("cp: ", node.cp, "\n")
-#     for link in node.links
-#         println(link, "\n")
-#     end
-# end
-
-########################################
-# TESTE COM INPUT VIA FILE
-#
-#
-#
+function deallocate(Gs::Array{Server, 1}, BandM::Array{Float64, 2}, substrate_network_changes::Array{Tuple{Int64, Int64, Float64}, 1}, first_host_key::Int64, first_host::Int64, results::Dict{Int64,Array{Float64, 1}})
+    for key in keys(results)
+        # if (key == first_host_key)
+        #     continue
+        # end
+        server = (key == first_host_key) ? first_host : index_of_higher_value_element(results[key])
+        println("SERVER", server, " -> ", Gs[server])
+        vm = pop!(Gs[server].VMs)
+        Gs[server].cp.atual += vm.cp
+        println("\t>>CP do server ", server, " -> ", Gs[server].cp.atual)
+        Gs[server].storage.atual += vm.storage
+        Gs[server].memory.atual += vm.memory
+        Gs[server].residual_band += Σ([link[2] for link in vm.links])
+        println("\tResetando server ", server)
+    end
+    for (src, dst, wght) in substrate_network_changes
+        BandM[src, dst] += wght
+    end
+end
 
 BandM = readdlm("BandM_fisico")
 
@@ -204,7 +221,7 @@ n_servers = size(Gs_data_from_file,1)
 
 Gs = Array{Server}(n_servers)
 for i=1:length(Gs)
-    Gs[i] = Server(Resource(0, 0, 0),Resource(0, 0, 0),Resource(0, 0, 0),[],0)
+    Gs[i] = Server(Resource(0, 0, 0),Resource(0, 0, 0),Resource(0, 0, 0),0,[],0)
 end
 for i=1:n_servers
     Gs[i].cp.top = Gs_data_from_file[i, 1]
@@ -218,9 +235,11 @@ for i=1:n_servers
     Gs[i].memory.residual = 0
     Gs[i].VMs = []
     Gs[i].custo_beneficio = 0
+    Gs[i].residual_band = Σ(BandM[i, :])
+    # println("server ", i, " -> band = ", Gs[i].residual_band)
 end
 
-println("\n", Gs, "\n", BandM, "\n")
+# println("\n", Gs, "\n", BandM, "\n")
 
 VNR = Array{VirtualMachine}(0)
 VNR_data_from_file = readdlm("VNR")
@@ -228,7 +247,7 @@ n_linhas = size(VNR_data_from_file, 1)
 n_colunas = size(VNR_data_from_file, 2)
 for i=1:n_linhas
     node = [Float64(k) for k in VNR_data_from_file[i, 1:n_colunas] if k != ""]
-    VM = VirtualMachine(node[1], node[2], node[3], [], 0.0)
+    VM = VirtualMachine(i, node[1], node[2], node[3], [], 0.0)
     deleteat!(node,[1,2,3])
     while length(node) > 0
         push!(VM.links, (node[1], node[2]))
@@ -239,45 +258,104 @@ end
 
 set_lrc_index(VNR)
 
-println("VNR => ", VNR, "\n")
+# println("VNR => ", VNR, "\n")
 
 sort!(VNR)
 
-println("VNR => ", VNR, "\n")
+# println("VNR => ", VNR, "\n")
 
 array_criterias = Dict("custo_beneficio" => Array{Float64}(0), "cp_residual" => Array{Float64}(0), "storage_residual" => Array{Float64}(0), "memory_residual" => Array{Float64}(0))
-PESOS = Dict("custo_beneficio" => 0.55, "cp_residual" => 0.15, "storage_residual" => 0.15, "memory_residual" => 0.15)
+PESOS = Dict("custo_beneficio" => 0.85, "cp_residual" => 0.05, "storage_residual" => 0.05, "memory_residual" => 0.05)
 
-
-for VM in VNR
+results = Dict{Int64, Array{Float64, 1}}()
+substrate_network_changes = Array{Tuple{Int64, Int64, Float64}, 1}()
+iteration = 1
+i=1
+first_host = -1
+first_host_key = -1
+while i <= length(VNR)
+    VM = VNR[i]
     println(VM)
+        println("@@ entrou no calc")
+        calc_custo_beneficio(Gs, BandM, VM)
+        calc_cp_residual(Gs, VM)
+        calc_storage_residual(Gs, VM)
+        calc_memory_residual(Gs, VM)
 
-    calc_custo_beneficio(Gs, BandM, VM)
-    calc_cp_residual(Gs, VM)
-    calc_storage_residual(Gs, VM)
-    calc_memory_residual(Gs, VM)
+    if (!haskey(results, VM.id))
+        array_criterias["custo_beneficio"] = [Float64(k.custo_beneficio) for k in Gs]
+        MAUT_norm_criteria_max(array_criterias["custo_beneficio"])
+        # quando tentando usar normalizacao de maximizacao, a banda de cada servidor fisico se esgota mais rapido
+        MAUT_marginalUtilty_custo_beneficio(array_criterias["custo_beneficio"])
 
-    array_criterias["custo_beneficio"] = [Float64(k.custo_beneficio) for k in Gs]
-    MAUT_norm_criteria_max(array_criterias["custo_beneficio"])
-    MAUT_marginalUtilty_custo_beneficio(array_criterias["custo_beneficio"])
-
-    array_criterias["cp_residual"] = [k.cp.residual for k in Gs]
-    MAUT_norm_criteria_max(array_criterias["cp_residual"])
-    MAUT_marginalUtility_cp_residual(array_criterias["cp_residual"])
-
-
-    array_criterias["storage_residual"] = [k.storage.residual for k in Gs]
-    MAUT_norm_criteria_max(array_criterias["storage_residual"])
+        array_criterias["cp_residual"] = [k.cp.residual for k in Gs]
+        MAUT_norm_criteria_min(array_criterias["cp_residual"])
+        MAUT_marginalUtility_cp_residual(array_criterias["cp_residual"])
 
 
-    array_criterias["memory_residual"] = [k.memory.residual for k in Gs]
-    MAUT_norm_criteria_max(array_criterias["memory_residual"])
+        array_criterias["storage_residual"] = [k.storage.residual for k in Gs]
+        MAUT_norm_criteria_min(array_criterias["storage_residual"])
 
-    result = MAUT_globalUtility(array_criterias, PESOS)
-    println(array_criterias, "\n", result, "\n")
 
-    selected_server = index_of_higher_value_element(result)
-    do_allocate(Gs, selected_server, VM)
+        array_criterias["memory_residual"] = [k.memory.residual for k in Gs]
+        MAUT_norm_criteria_min(array_criterias["memory_residual"])
+
+        current_result = MAUT_globalUtility(array_criterias, PESOS)
+        
+        println("-------------------------\n",array_criterias, "\n", current_result, "\n-------------------------")
+
+        push!(results, VM.id => current_result)        
+    end
+    println("Alocando VM ", VM.id)
+
+    selected_server = index_of_higher_value_element(haskey(results, VM.id) ? results[VM.id] : current_result)
+    println("Server selecionado: ", selected_server)
+    if i == 1
+        first_host = selected_server
+        println("primeira iteracao, setando first_host and first_host_key\n.\n.\n.")
+        first_host_key = VM.id
+        results[VM.id][selected_server] = -1
+    end
+    println("Resultados para VM ", VM.id, " : ", results[VM.id])
+    if !do_allocate(Gs, selected_server, VM)
+        if (Σ(results[first_host_key]) == -length(results[first_host_key]))
+            println("Fail to allocate VNR")
+            deallocate(Gs, BandM, substrate_network_changes, first_host_key, first_host, results)
+            return 0
+        end
+        println("Alocacao falhou, iniciando processo de desalocacao\n")
+        deallocate(Gs, BandM, substrate_network_changes, first_host_key, first_host, results)
+        delete!(results, VM.id)
+        for key in keys(results)
+            if(key != first_host_key)
+                delete!(results, key)
+            end
+        end
+        deleteat!(substrate_network_changes, 1:length(substrate_network_changes))
+        println("Fim do processo de desalocacao")
+        # println(results)
+        # return
+        i = 1
+    else
+        link_path = Array{Int64, 1}()
+        if (i > 1)
+            for link in VM.links
+                if (haskey(results, link[1])) #if the node dst is already allocated
+                    host_src = index_of_higher_value_element(results[VM.id])
+                    host_dst = (link[1] == first_host_key) ? first_host : index_of_higher_value_element(results[link[1]])
+                    link_path = dijkstra(BandM, host_src, host_dst, link[2])
+                    println(">> PATH FROM ", host_src, " TO ", host_dst, " : ", link_path)
+                    for node in link_path
+                        BandM[host_src, node] -= link[2]
+                        push!(substrate_network_changes, (host_src, node, link[2]))
+                        host_src = node
+                    end
+                end
+            end
+            println("band allocated\n", BandM)
+        end
+        i += 1
+    end
 end
 for i=1:length(Gs)
     println("\nSERVER ", i, "\n---------------")
@@ -285,5 +363,6 @@ for i=1:length(Gs)
     println("storage top: ", Gs[i].storage.top, "\nstorage atual: ", Gs[i].storage.atual, "\nstorage residual: ", Gs[i].storage.residual)
     println("memory top: ", Gs[i].memory.top, "\nmemory atual: ", Gs[i].memory.atual, "\nmemory residual: ", Gs[i].memory.residual)
     println("custo beneficio: ", Gs[i].custo_beneficio)
+    println("banda residual: ", Gs[i].residual_band)
     println("VMs: ", Gs[i].VMs)
 end
---
+++ dijkstra.jl
@@ -0,0 +1,74 @@
+function index_of_not_visited_lowest_value_element(array::Array{Float64}, visited::Array{Int64, 1})
+    index = 0
+    lowest_value = 999999
+    for i=1:length(array)
+        if (array[i] < lowest_value && visited[i] == 0)
+            index = i
+            lowest_value = array[i]
+        end
+    end
+    return index
+end
+function dijkstra(Graph::Array{Float64,2}, src::Int64, dst::Int64, desired_band::Float64)
+    n_vertices = size(Graph, 1)
+    dist = Array{Float64, 1}()
+    visited = Array{Int64, 1}()
+    precedents = Array{Int64}[[] for k in 1:n_vertices]
+    path = Array{Int64, 1}()
+    for i=1:n_vertices
+        push!(dist, 999999.0)
+        push!(visited, 0.0)
+    end
+    dist[src] = 0
+    for i=1:n_vertices
+        u = index_of_not_visited_lowest_value_element(dist, visited)
+        if (u==0)
+            continue
+        end
+        visited[u] = 1
+        for j=1:n_vertices
+            if ((visited[j]==0) && (Graph[u, j] != 0) && (dist[u] != 999999) && (dist[u] + Graph[u,j] <= dist[j]))
+                if (Graph[u, j] >= desired_band) 
+                    if (dist[u] + Graph[u, j] == dist[j])
+                        push!(precedents[j], u)
+                    else
+                        splice!(precedents[j], 1:length(precedents[j]), u)
+                    end
+                    dist[j] = dist[u] + Graph[u, j];
+                else
+                    visited[j]=1
+                end
+            end
+        end
+    end
+    while(dst != src)
+        unshift!(path, dst)
+        dst = precedents[dst][1]#just take the first one
+    end
+    return path
+    # println("VERTICES   : ");
+    # for i = 1:n_vertices
+    #     println(i, " ");
+    # end
+    # println("\n");
+    # println("ESTIMATIVAS: ");
+    # for i = 1:n_vertices
+    #     println(dist[i], " ");
+    # end
+    # println("\n");
+    # println("PRECEDENTES:\n");
+    # for i = 1:n_vertices
+    #     println("Precedente(s) vertice ", i, ": ");
+    #     println(precedents[i])
+    #     println("\n");
+    # end
+
+    # println("\nDistancias:\n");
+    # for i = 1:n_vertices
+    #     println("Vertice ", src," ao vertice ", i," : ", dist[i]);
+    # end
+end
+
+# Graph = readdlm("BandM_fisico")
+# path = dijkstra(Graph, 1, 3, 25.0)
+# println(path)
